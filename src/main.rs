ed7a352b55889d40d1425ba0a0bd5c981cf769aa
Tuxium <ursusfeline07@gmail.com>
2026-02-04 13:55:13 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@ fn main() -> Result<(),Box<dyn std::error::Error>> {
     let raw = std::process::Command::new("cargo")
         .arg("metadata")
         .arg("--all-features")
-        .output()
-        .expect("failed to execute cargo");
+        .output()?;
 
-    let raw = String::from_utf8(raw.stdout)
-        .expect("failed to read metadata");
+    let raw = String::from_utf8(raw.stdout)?;
 
     let metadata:Metadata = raw.parse()?;
 
